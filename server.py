f181eb061eb49ca1dad6f5ae3ae99add05072505
Tarasov Nick <nick.tarasov@emc.com>
2019-01-25 20:26:35 +0300

    Updates in server

--- server.py
+++ server.py
@@ -2,8 +2,7 @@
 # он ждет подключения, обрабатывая команды / либо команды вводятся с клавиатуры
 import socket
 
-
-commands = {0: "STOP_SERVER", 1: "RUN_TEST1"}
+commands = {"STOP_SERVER": 0, "RUN_TEST1": 1}
 
 run_server = True
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,19 +12,18 @@ udp_socket.bind(("", server_port))
 while run_server:
     print("Server actions: ")
     for key, command in commands.items():
-        print("command {1}: {0}".format(command, key))
+        print("command {0}: {1}".format(command, key))
 
     command = input("Input command: ")
-    if command == commands[0]:
+    if command == commands["STOP_SERVER"]:
         run_server = False
 
-    if command == commands[1]:
+    if command == commands["RUN_TEST1"]:
         print("Тест 1.")
         print("Скорость UDP соединения в зависимости от расстояния.")
         distance = [5, 10, 15, 20]
         print("Запустите Test1 на клиенте.")
         for dist in distance:
-
             print("Сервер UDP запущен и ожидает данных на порту {0}".format(server_port))
 
             buf_size, address = udp_socket.recvfrom(1024)
