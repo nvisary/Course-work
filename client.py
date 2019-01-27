5ac1e7e7f34e85f4acc5bf7c0848ea7844dfa041
nvisary <nvisary@gmail.com>
2019-01-27 19:48:39 +0300

    Updates

--- client.py
+++ client.py
@@ -1,26 +1,35 @@
 import socket
 import time
+BUF_SIZE = 4096
 
 run_client = True
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-commands = {"STOP_CLIENT": 0,  "RUN_TEST1": 1}
+commands = {"STOP_CLIENT": 0, "RUN_TEST1": 1, "PRINT_GRAPHICS": 2}
 img = open("img/cat.bmp", "rb")
+img_size = len(img.read())
+img.seek(0)
 
+speeds_udp = []
+times_udp = []
+distance = [5, 10, 15, 20]
 
 while run_client:
     print("Client actions:")
     for key, command in commands.items():
         print("command {0}: {1}".format(command, key))
 
+    print("******\nData: ")
+    print("UDP\nSpeeds: ", speeds_udp)
+    print("Times: ", times_udp)
+    print("******")
+
     command = int(input("Input command: "))
     if command == commands["STOP_CLIENT"]:
         run_client = False
+        udp_socket.close()
 
     if command == commands["RUN_TEST1"]:
-        speeds = []
-        times = []
-        distance = [5, 10, 15, 20]
         print("Тест 1.")
         print("Скорость UDP соединения в зависимости от расстояния.")
         print("Запустите Test1 на сервере.")
@@ -36,11 +45,10 @@ while run_client:
             print("Сервер готов.")
             print("Отправляю картинку.")
             start_time = time.time()
-            BUF_SIZE = 4096
+
+            count_parts = img_size // BUF_SIZE
             parts = 0
             img.seek(0)
-            img_size = 3.33 * 1000000
-            count_parts = img_size / BUF_SIZE
             while True:
                 part = img.read(BUF_SIZE)
                 udp_socket.sendto(part, (server_ip, server_port))
@@ -54,12 +62,10 @@ while run_client:
             print("start time: ", start_time)
             print("end time: ", end_time)
             if send_time:
-                speed = BUF_SIZE * parts/ send_time
+                speed = BUF_SIZE * parts / send_time
             else:
                 speed = 0
-            speeds.append(speed)
-            times.append(send_time)
+            speeds_udp.append(speed)
+            times_udp.append(send_time)
             print("Затраченное на отправку время: ", send_time)
             print("Скорость: {0} Б/Сек".format(speed))
-        print(speeds)
-        print(times)
--- util.py
+++ util.py
@@ -1,9 +1,13 @@
+BUFF_SIZE = 4096
+
 def recv_all(sock):
-    BUFF_SIZE = 4096
     data = b''
+    count = 0
     while True:
         part, address = sock.recvfrom(BUFF_SIZE)
+        count += 1
         data += part
+        print(count)
         if len(part) < BUFF_SIZE:
             break
     return data
