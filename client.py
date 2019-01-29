3d9278d6d0ffc2e84e57756fbbdcf851f4ca7182
nvisary <nvisary@gmail.com>
2019-01-29 19:11:09 +0300

    Updates add test2 and graphics

--- client.py
+++ client.py
@@ -1,17 +1,22 @@
 import socket
 import time
+import matplotlib.pyplot as plt
+
 BUF_SIZE = 4096
 
 run_client = True
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-commands = {"STOP_CLIENT": 0, "RUN_TEST1": 1, "PRINT_GRAPHICS": 2}
+commands = {"STOP_CLIENT": 0, "RUN_TEST1": 1, "RUN_TEST2": 2, "PRINT_GRAPHICS": 3}
 img = open("img/cat.bmp", "rb")
 img_size = len(img.read())
 img.seek(0)
 
 speeds_udp = []
 times_udp = []
+times_tcp = []
+speeds_tcp = []
 distance = [5, 10, 15, 20]
 
 while run_client:
@@ -22,6 +27,8 @@ while run_client:
     print("******\nData: ")
     print("UDP\nSpeeds: ", speeds_udp)
     print("Times: ", times_udp)
+    print("TCP\nSpeeds: ", speeds_tcp)
+    print("Times: ", times_tcp)
     print("******")
 
     command = int(input("Input command: "))
@@ -33,7 +40,8 @@ while run_client:
         print("Тест 1.")
         print("Скорость UDP соединения в зависимости от расстояния.")
         print("Запустите Test1 на сервере.")
-
+        speeds_udp = []
+        times_udp = []
         server_ip = input("Введите ip-адресс UDP сервера: ")
         server_port = int(input("Введите порт UDP сервера: "))
 
@@ -41,8 +49,6 @@ while run_client:
             print("Отойдите на расстояние в {0} метров от точки доступа.".format(dist))
             input("Для продолжения нажмите enter")
             print("Начинаю тестирование.")
-            print("Ожидаю готовности от сервера.")
-            print("Сервер готов.")
             print("Отправляю картинку.")
             start_time = time.time()
 
@@ -62,10 +68,50 @@ while run_client:
             print("start time: ", start_time)
             print("end time: ", end_time)
             if send_time:
-                speed = BUF_SIZE * parts / send_time
+                speed = BUF_SIZE * parts / send_time / 1024 / 1024
             else:
                 speed = 0
             speeds_udp.append(speed)
             times_udp.append(send_time)
             print("Затраченное на отправку время: ", send_time)
             print("Скорость: {0} Б/Сек".format(speed))
+    if command == commands["RUN_TEST2"]:
+        print("Тест 2.")
+        print("Скорость TCP соединения в зависимости от расстояния.")
+        print("Запустите Test2 на сервере.")
+        server_ip = input("Введите ip-адресс TCP сервера: ")
+        server_port = int(input("Введите порт TCP сервера: "))
+        tcp_socket.connect((server_ip, server_port))
+        print("Соединение установлено.")
+        for dist in distance:
+            print("Отойдите на расстояние в {0} метров от точки доступа.".format(dist))
+            input("Для продолжения нажмите enter")
+            print("Начинаю тестирование.")
+            print("Отправляю картинку.")
+            img.seek(0)
+            start_time = time.time()
+            tcp_socket.send(img.read())
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            speed = img_size / elapsed_time / 1024 / 1024
+            times_tcp.append(elapsed_time)
+            speeds_tcp.append(speed)
+            print("Затраченное на отправку время: ", elapsed_time)
+            print("Скорость: {0} Mб/Сек".format(speed))
+    if command == commands["PRINT_GRAPHICS"]:
+        if len(times_udp) == len(distance) == len(speeds_udp) == len(speeds_tcp):
+            plt.title("Скорость от расстояния")
+            plt.xlabel("Расстояние")
+            plt.ylabel("Скорость передачи Мб/с")
+            plt.plot(distance, speeds_udp, label="speed UDP")
+            plt.plot(distance, speeds_tcp, label="speed TCP")
+            plt.legend()
+            plt.show()
+
+            plt.title("Время передачи от расстояния")
+            plt.xlabel("Расстояние")
+            plt.ylabel("Время передачи с")
+            plt.plot(distance, times_udp, label="time UDP")
+            plt.plot(distance, times_tcp, label="time TCP")
+            plt.legend()
+            plt.show()
--- server.py
+++ server.py
@@ -2,15 +2,26 @@
 # он ждет подключения, обрабатывая команды / либо команды вводятся с клавиатуры
 import socket
 import util
-commands = {"STOP_SERVER": 0, "RUN_TEST1": 1}
+import matplotlib.pyplot as plt
+
+commands = {"STOP_SERVER": 0, "RUN_TEST1": 1, "RUN_TEST2": 2, "PRINT_GRAPHICS": 3}
 
 run_server = True
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_port = int(input("Введите порт UDP сервера: "))
-udp_socket.bind(("", server_port))
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_tcp_port = int(input("Введите порт TCP сервера: "))
+server_udp_port = int(input("Введите порт UDP сервера: "))
+
+udp_socket.bind(("", server_udp_port))
+tcp_socket.bind(("", server_tcp_port))
+tcp_socket.listen(1)
 
 img = open("img/cat.bmp", "rb")
 img_size = len(img.read())
+
+lost_packets = []
+distance = [5, 10, 15, 20]
 
 while run_server:
     print("Server actions: ")
@@ -24,18 +35,40 @@ while run_server:
     if command == commands["RUN_TEST1"]:
         print("Тест 1.")
         print("Скорость UDP соединения в зависимости от расстояния.")
-        distance = [5, 10, 15, 20]
         print("Запустите Test1 на клиенте.")
+
         for dist in distance:
-            print("Сервер UDP запущен и ожидает данных на порту {0}".format(server_port))
+            print("Сервер UDP запущен и ожидает данных на порту {0}".format(server_udp_port))
             count_parts = img_size // util.BUFF_SIZE
 
-            file_data, lost_packets = util.recv_all(udp_socket, count_parts)
+            file_data, lost = util.recv_all(udp_socket, count_parts)
             udp_socket.settimeout(100)
-            print("Lost: ", lost_packets)
+            lost_packets.append(lost)
+            print("Lost: ", lost)
             f = open("received.bmp", "wb")
             f.write(file_data)
             f.close()
             print("Изображение принято и сохранено.")
 
+    if command == commands["RUN_TEST2"]:
+        print("Тест 2.")
+        print("Скорость TCP соединения в зависимости от расстояния")
+        print("Сервер TCP запущен и ожидает данных на порту {0}".format(server_tcp_port))
+        print("Запустите Test2 на клиенте.")
+        connection, address = tcp_socket.accept()
 
+        for dist in distance:
+
+            file_data = connection.recv(img_size)
+            f = open("tcp_received{0}.bmp".format(dist), "wb")
+            f.write(file_data)
+            f.close()
+
+
+    if command == commands["PRINT_GRAPHICS"]:
+        if len(lost_packets) == len(distance):
+            plt.title("UDP потери от расстояния")
+            plt.xlabel("Расстояние м")
+            plt.ylabel("Потерянные пакеты %")
+            plt.plot(distance, lost_packets)
+            plt.show()
